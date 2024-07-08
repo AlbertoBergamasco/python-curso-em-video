2abacaac673f342d308b41913fc84ce1d2a15d6c
Alberto Bergamasco <131218934+AlbertoBergamasco@users.noreply.github.com>
2024-07-08 18:08:46 -0300

    Conclusão de mais um desafio
    Criação do desafio 4

--- mundo 1/desafio2.py
+++ mundo 1/desafio2.py
@@ -1,5 +1,7 @@
-dia = input('Qual a sua data de nascimento? dia?')
-mes = input('mês ?')
-ano = input('ano ?')
 
-print('O seu nascimento foi: '+dia+'/'+mes+'/'+ano)
+n1 = int(input('Qual o valor de n1'))
+n2 = int(input('Qual o valor de n2'))
+
+#print('A soma10 é {}'.format(n1+n2))
+
+print('A soma entre {} e {} vale {}'.format(n1, n2, n1+n2))
---
+++ mundo 1/desafio4.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+
+soma = n1 + n2
+
+print(type(soma), soma)
+
+
