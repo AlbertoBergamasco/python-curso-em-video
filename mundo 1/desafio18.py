7f7e5f162e0c32d3d9ef93e5f6eb4482030ffa7a
Alberto Bergamasco <131218934+AlbertoBergamasco@users.noreply.github.com>
2024-07-23 13:22:39 -0300

    Update desafio18.py
    usando hypot

--- mundo 1/desafio18.py
+++ mundo 1/desafio18.py
@@ -1,8 +1,8 @@
-import math
+from math  import hypot
 
 c1 = float(input('Digite o primeiro cateto: '))
 c2 = float(input('Digite o segundo cateto: '))
 
-h = math.sqrt((c1*c1)+(c2*c2))
+h = hypot(c1, c2)
 print('Com os valores c1:{:.2f}, c2:{:.2f} '.format(c1, c2))
 print('A hipotenusa vai medir {:.2f}'.format(h))
